3e342706e397bc7af4ff9737a634bcb7923d9a05
David Morrow <quickbasic44@outlook.com>
2021-10-11 21:37:00 -0400

    working matrices

--- Matrices/Form1.Designer.cs
+++ Matrices/Form1.Designer.cs
@@ -30,40 +30,92 @@ namespace Matrices
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(176, 206);
+            this.groupBox1.Location = new System.Drawing.Point(209, 77);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(549, 155);
+            this.groupBox1.Size = new System.Drawing.Size(155, 104);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Matrix A";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(495, 513);
+            this.button1.Location = new System.Drawing.Point(498, 517);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 42);
+            this.button1.Size = new System.Drawing.Size(130, 52);
             this.button1.TabIndex = 2;
             this.button1.Text = "Create Matrix";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(257, 517);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Initial Matrix";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1277, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1277, 626);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +123,10 @@ namespace Matrices
 
         private System.Windows.Forms.GroupBox groupBox1;
         public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
     }
 }
 
--- Matrices/Form1.cs
+++ Matrices/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,10 +17,10 @@ namespace Matrices
         public int Columns { get; set; }
         public Matrix matrix;
         MyData data;
-        int width = 0;
-        int height = 0;
         int total = 0;
-        Label[] labelArray;
+        Label[,] labelArray;
+        int locationWidth = 0;
+        int locationHeight = 0;
 
 
 
@@ -36,68 +37,160 @@ namespace Matrices
         private void button1_Click(object sender, EventArgs e)
         {
             matrix.ShowDialog();
+            label1.Text = "25.522";
+           
                         
         }
 
-        private void SizeGroupBox(int matrixWidth, int matrixHeight)
+        private void SizeGroupBox()
         {
-           int width = matrixHeight * 150;
-            int height = matrixHeight * 100;
-            groupBox1.Size = new Size(width, height);
-        }
-
-        private void SetLabelLocations(int matrixWidth, int matrixHeight)
-        {
-            int width = matrixHeight * 150;
-            int height = matrixHeight * 100;
-            groupBox1.Size = new Size(width, height);
-        }
-
-        private void CheckLabelWidthsAfterSettingNumber(int matrixWidth, int matrixHeight)
-        {
-            int width = matrixHeight * 150;
-            int height = matrixHeight * 100;
-            groupBox1.Size = new Size(width, height);
-        }
-
-        private void CreateLabelsFromMyData()
-        {
-            if (data != null)
+            int width;
+            int height;
+            switch (Columns)
             {
-                width = data.Rows;
-                height = data.Cols;
-                total = width * height;
-                labelArray = new Label[total];
+                case 1: 
+                width = 200;
+                     break;
+                case 2:
+                width = 500;
+                    break;
+                case 3:
+                    width = 700;
+                    break;
+                case 4:
+                    width = 900;
+                    break;
+                case 5:
+                    width = 1200;
+                    break;
+                default:
+                    width = 1600;
+                    break;
 
-                for(int i = 1;i <= total;i++)
-                {
-                    labelArray[i] = new Label();
-                    this.Controls.Add(labelArray[i]);
-                    this.groupBox1.Controls.Add(labelArray[i]);
+            }
+            switch (Rows)
+            {
+                case 1:
+                    height = 200;
+                    break;
+                case 2:
+                    height = 300;
+                    break;
+                case 3:
+                    height = 400;
+                    break;
+                case 4:
+                    height = 800;
+                    break;
+                case 5:
+                    height = 1200;
+                    break;
+                default:
+                    height = 1200;
+                    break;
+            }
+            groupBox1.Size = new Size(width, height);
+        }
+   
+        private void SetLabelLocations()
+        {
+            
+                locationWidth = (groupBox1.Width - 100) / Columns;
+                int widthIncrement = locationWidth;
+                locationWidth -= widthIncrement;
+                locationHeight = (groupBox1.Height - 100) / Rows;
+                int heightIncrement = locationHeight;
+                locationHeight -= heightIncrement;
+                           
+                 for (int i = 0;i < Rows;i++)
+                 {
+                      locationWidth = 50;
+                      locationHeight += heightIncrement;
+                    for (int j = 0;j < Columns;j++)
+                    {
+
+                        labelArray[i, j].Top = locationHeight;
+                        labelArray[i, j].Left = locationWidth;
+                    labelArray[i, j].Text = (i * j).ToString();
+                    locationWidth += widthIncrement;
+                    Debug.WriteLine(labelArray[i, j].Text);
+                    Debug.WriteLine(labelArray[i, j].Location.ToString());
+
+
+                    }
+                 }
+
+            //MessageBox.Show(labelArray.Length.ToString());
+
+            
+        }
+
+        private void CheckLabelWidthsAfterSettingNumber()
+        {
+            int width = 150;
+            int height = 150;
+            groupBox1.Size = new Size(width, height);
+        }
+
+        private void CreateLabels()
+        {
+            Font font = new Font("Arial", 12);
+            if (data != null & Rows > 0)
+            {
+
+                total = Rows * Columns;
+                labelArray = new Label[Rows, Columns];
+                Label label;
+
+                for(int i = 0;i < Rows;i++)
+                {   
+                    for (int j = 0;j < Columns; j++)
+                    {
+                        label = new Label();
+                        label.Font = font;
+                        label.Name = "label" + i;
+                        label.Text = i.ToString();
+                        labelArray[i, j] = label;
+                        this.groupBox1.Controls.Add(label);
+                    }
                 }
-                //this.Controls.AddRange(new Control[] { labelArray[0], labelArray[1] });
-
-
-
+                
+                
+             //this.Controls.AddRange(new Control[] { labelArray[0], labelArray[1] });
             }
         }
 
-        private void SetLabelFontSize(int matrixWidth, int matrixHeight)
+        private void SetLabelFontSize()
         {
-            int width = matrixHeight * 150;
-            int height = matrixHeight * 100;
-            groupBox1.Size = new Size(width, height);
+           
+            
+        }
+
+        private void ClearGroupBox()
+        {
+            groupBox1.Controls.Clear();
         }
 
 
 
-        private void Form1_Activated(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
             if (data != null)
             {
-                //label9.Text = data.Rows.ToString();
-                //label11.Text = data.Cols.ToString();
+                Rows = data.Rows;
+                Columns = data.Cols;
+                ClearGroupBox();
+                SizeGroupBox();
+                CreateLabels();
+                SizeGroupBox();
+                SetLabelLocations();
+
+
             }
+        }
+
+        private void label1_MouseClick(object sender, MouseEventArgs e)
+        {
             
         }
     }
